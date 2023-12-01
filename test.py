2e2c60fc461c82e51a221d2991e80d67aacda7a1
Kareemcanaan <can3an32@gmail.com>
2023-12-01 17:44:26 +0000

    final submission for cw 2

---
+++ test.py
@@ -0,0 +1,45 @@
+# Import the required libraries
+from tkinter import *
+from PIL import Image, ImageTk
+
+# Create an instance of tkinter frame
+win = Tk()
+
+# Set the size of the tkinter window
+win.geometry("700x350")
+
+# Define a Canvas widget
+canvas = Canvas(win, width=600, height=400, bg="white")
+canvas.pack(pady=20)
+
+# Add Images to Canvas widget
+image = ImageTk.PhotoImage(Image.open('images/pship.png'))
+img = canvas.create_image(250, 120, anchor=NW, image=image)
+
+def left(e):
+   x = -20
+   y = 0
+   canvas.move(img, x, y)
+
+def right(e):
+   x = 20
+   y = 0
+   canvas.move(img, x, y)
+
+def up(e):
+   x = 0
+   y = -20
+   canvas.move(img, x, y)
+
+def down(e):
+   x = 0
+   y = 20
+   canvas.move(img, x, y)
+
+# Bind the move function
+win.bind("<Left>", left)
+win.bind("<Right>", right)
+win.bind("<Up>", up)
+win.bind("<Down>", down)
+
+win.mainloop()
